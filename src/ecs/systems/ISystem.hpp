b5707dc8839c768ebf63e67eeafa532d44f543a6
hle-hena <53910555+hle-hena@users.noreply.github.com>
2026-02-22 18:35:49 +0100

    Added a pipeline map class (#22)
    * Added a new PipelineMap class
    
    * Added an implementation of the pipelineMap class for the render system
    
    * Added a config class for the pipeline map, and made the call for configuration be callbacks instead of in class function
    
    * Added the implementation for the camera render system
    
    * Added an return to the getShaderInfo function

--- src/ecs/systems/ISystem.hpp
+++ src/ecs/systems/ISystem.hpp
@@ -5,7 +5,7 @@
 /*  Project: Hel Engine                                                       */
 /*  Created: 2026/02/16 14:44:05 by hle-hena                                  */
 /*                                                                            */
-/*  Last Modified: 2026/02/19 18:58:42                                        */
+/*  Last Modified: 2026/02/22 18:33:16                                        */
 /*             By: hle-hena                                                   */
 /*                                                                            */
 /*    -----                                                                   */
