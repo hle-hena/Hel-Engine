3be116b633cb300922cfeb8c7af7646ca533d97c
hle-hena <hugoleh@icloud.com>
2026-02-27 17:57:15 +0100

    Fixed Ui.cpp

--- src/ecs/systems/core/UI.cpp
+++ src/ecs/systems/core/UI.cpp
@@ -5,7 +5,7 @@
 /*  Project: Hel Engine                                                       */
 /*  Created: 2026/02/27 11:06:43 by hle-hena                                  */
 /*                                                                            */
-/*  Last Modified: 2026/02/27 15:32:29                                        */
+/*  Last Modified: 2026/02/27 17:57:07                                        */
 /*             By: hle-hena                                                   */
 /*                                                                            */
 /*    -----                                                                   */
@@ -17,11 +17,7 @@
 
 #include "ecs/systems/core/UI.hpp"
 #include "ecs/Registry.hpp"
-#include "core/Engine.hpp"
-#include "platform/window/Window.hpp"
-#include "api/vulkan/Device.hpp"
-#include "api/vulkan/VulkanInstance.hpp"
-#include "api/vulkan/Swapchain.hpp"
+#include "api/ImGui/imgui.h"
 
 namespace	hel::sys {
 
