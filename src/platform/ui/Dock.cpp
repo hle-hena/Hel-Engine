7600b697be63fb0e5c320124b8f4f3f30b415abd
hle-hena <hugoleh@icloud.com>
2026-03-17 23:34:14 +0100

    Made the code cleaner to read

--- src/platform/ui/Dock.cpp
+++ src/platform/ui/Dock.cpp
@@ -5,7 +5,7 @@
 /*  Project: Hel Engine                                                       */
 /*  Created: 2026/03/16 10:31:03 by hle-hena                                  */
 /*                                                                            */
-/*  Last Modified: 2026/03/17 21:29:44                                        */
+/*  Last Modified: 2026/03/17 23:33:36                                        */
 /*             By: hle-hena                                                   */
 /*                                                                            */
 /*    -----                                                                   */
@@ -22,33 +22,24 @@ namespace	hel::sys {
 
 void	Dock::split(Splitter::Dir dir, IPanel *splitPanel) {
 	_type = Type::Split;
+	_splitDir = (dir == Splitter::Dir::Left) ? Splitter::Dir::Right :
+				(dir == Splitter::Dir::Up) ? Splitter::Dir::Down : dir;
 
-	if (dir == Splitter::Dir::Left || dir == Splitter::Dir::Right) {
-		_childOne = std::unique_ptr<Dock>(new Dock(_dockName + "_leftChild"));
-		_childTwo = std::unique_ptr<Dock>(new Dock(_dockName + "_rightChild"));
-	} else {
-		_childOne = std::unique_ptr<Dock>(new Dock(_dockName + "_upChild"));
-		_childTwo = std::unique_ptr<Dock>(new Dock(_dockName + "_downChild"));
-	}
+	bool	isVertical = (_splitDir == Splitter::Dir::Right);
+	_childOne = std::make_unique<Dock>(_dockName +
+									(isVertical ? "_left" : "_up"));
+	_childTwo = std::make_unique<Dock>(_dockName +
+									(isVertical ? "_right" : "_down"));
 
-	Dock	*target = nullptr;
-	Dock	*keep = nullptr;
-	if (dir == Splitter::Dir::Left || dir == Splitter::Dir::Up) {
-		target = _childOne.get();
-		keep = _childTwo.get();
-	} else {
-		target = _childTwo.get();
-		keep = _childOne.get();
-	}
+	Dock	*target = (dir == Splitter::Dir::Left || dir == Splitter::Dir::Up)
+						? _childOne.get() : _childTwo.get();
+	Dock	*keep = (target == _childOne.get()) ? _childTwo.get()
+												: _childOne.get();
 
 	splitPanel->changeOwner(target);
 	for (auto panel: _panels)
 		panel->changeOwner(keep);
 	_panels.clear();
-
-	_splitDir = dir;
-	if (_splitDir == Splitter::Dir::Left)
-		_splitDir = Splitter::Dir::Right;
 }
 
 void	Dock::merge(void) {
@@ -57,38 +48,7 @@ void	Dock::merge(void) {
 
 void	Dock::render(Window *window, ImVec2 size) {
 	if (_type == Type::Split) {
-		ImVec2	origin = ImGui::GetCursorScreenPos();
-		bool	splitVertical = (_splitDir == Splitter::Dir::Left ||
-								_splitDir == Splitter::Dir::Right);
-
-		ImVec2	pos = splitVertical ?
-						ImVec2(origin.x + size.x * _splitRatio, origin.y) :
-						ImVec2(origin.x, origin.y + size.y * _splitRatio);
-		float	splitterSize = splitVertical ? size.y : (size.x);
-		Splitter(&_splitRatio)
-			.setLabel((_dockName + "_splitter").c_str())
-			.setDir(_splitDir)
-			.setMin(0.f)
-			.setMax(1.f)
-			.setPos(pos)
-			.setNormalizer(splitVertical ? size.x : size.y)
-			.setSize(splitterSize)
-			.build();
-
-		auto	childOneSize = splitVertical ?
-									ImVec2(size.x * _splitRatio, size.y) :
-									ImVec2(size.x, size.y * _splitRatio);
-		auto	childTwoSize = splitVertical ?
-									ImVec2(size.x - childOneSize.x, size.y) :
-									ImVec2(size.x, size.y - childOneSize.y);
-		ImVec2	startPos = ImGui::GetCursorScreenPos();
-
-		_childOne->render(window, childOneSize);
-		if (splitVertical)
-			ImGui::SetCursorScreenPos({startPos.x + childOneSize.x, startPos.y});
-		else
-			ImGui::SetCursorScreenPos({startPos.x, startPos.y + childOneSize.y});
-		_childTwo->render(window, childTwoSize);
+		renderSplits(window, size);
 	} else {
 		ImGui::BeginChild(_dockName.c_str(), size, ImGuiChildFlags_Borders);
 		renderDragDrop();
@@ -97,65 +57,90 @@ void	Dock::render(Window *window, ImVec2 size) {
 	}
 }
 
-void	Dock::renderDragDrop(void) {
-	if (auto payload = ImGui::GetDragDropPayload()) {
-		if (!payload->IsDataType("TAB_MOVE"))
-			return ;
-	}
-	else
-		return ;
-
-	ImVec2	avail = ImGui::GetContentRegionAvail();
+void	Dock::renderSplits(Window *window, ImVec2 size) {
 	ImVec2	origin = ImGui::GetCursorScreenPos();
-	float	tabBarH = ImGui::GetFrameHeight();
+	bool	isVertical = (_splitDir == Splitter::Dir::Right);
 
-	ImVec2	topLeft = {origin.x, origin.y + tabBarH};
-	ImVec2	topRight = {origin.x + avail.x, origin.y + tabBarH};
-	ImVec2	bottomLeft = {origin.x, origin.y + avail.y};
-	ImVec2	bottomRight = {origin.x + avail.x, origin.y + avail.y};
-	ImVec2	center = {origin.x + avail.x * 0.5f,
+	Splitter(&_splitRatio)
+		.setLabel((_dockName + "_splitter").c_str())
+		.setPos(isVertical ? ImVec2(origin.x + size.x * _splitRatio, origin.y)
+						: ImVec2(origin.x, origin.y + size.y * _splitRatio))
+		.setNormalizer(isVertical ? size.x : size.y)
+		.setSize(isVertical ? size.y : size.x)
+		.setMin(0.f).setMax(1.f)
+		.setDir(_splitDir)
+		.build();
+
+	auto	oneSize = isVertical ? ImVec2(size.x * _splitRatio, size.y)
+								: ImVec2(size.x, size.y * _splitRatio);
+	auto	twoSize = isVertical ? ImVec2(size.x - oneSize.x, size.y)
+								: ImVec2(size.x, size.y - oneSize.y);
+	ImVec2	start = ImGui::GetCursorScreenPos();
+
+	_childOne->render(window, oneSize);
+	ImGui::SetCursorScreenPos(isVertical ? ImVec2(start.x + oneSize.x, start.y)
+										: ImVec2(start.x, start.y + oneSize.y));
+	_childTwo->render(window, twoSize);
+}
+
+Dock::RenderDragDropContext::RenderDragDropContext(void) {
+	ImVec2	avail = ImGui::GetContentRegionAvail();
+	origin = ImGui::GetCursorScreenPos();
+	mouse = ImGui::GetMousePos();
+	tabBarH = ImGui::GetFrameHeight();
+
+	topLeft = {origin.x, origin.y + tabBarH};
+	topRight = {origin.x + avail.x, origin.y + tabBarH};
+	bottomLeft = {origin.x, origin.y + avail.y};
+	bottomRight = {origin.x + avail.x, origin.y + avail.y};
+	center = {origin.x + avail.x * 0.5f,
 		origin.y + tabBarH + (avail.y - tabBarH) * 0.5f};
 
-	struct	Zone {
-		ImVec2			a, b, c;
-		Splitter::Dir	dir;
-	};
+	released = ImGui::IsMouseReleased(ImGuiMouseButton_Left);
+}
 
+bool	Dock::renderTriangleZones(const RenderDragDropContext &ctx,
+								ImDrawList *draw, IPanel *panel) {
+	struct	Zone { ImVec2	a, b, c;	Splitter::Dir	dir; };
 	Zone	zones[] = {
-		{topLeft, topRight, center, Splitter::Dir::Up},
-		{topRight, bottomRight, center, Splitter::Dir::Right},
-		{bottomRight, bottomLeft, center, Splitter::Dir::Down},
-		{bottomLeft, topLeft, center, Splitter::Dir::Left}
+		{ctx.topLeft, ctx.topRight, ctx.center, Splitter::Dir::Up},
+		{ctx.topRight, ctx.bottomRight, ctx.center, Splitter::Dir::Right},
+		{ctx.bottomRight, ctx.bottomLeft, ctx.center, Splitter::Dir::Down},
+		{ctx.bottomLeft, ctx.topLeft, ctx.center, Splitter::Dir::Left}
 	};
-
-	ImVec2	mousePos = ImGui::GetMousePos();
-	auto	draw = ImGui::GetForegroundDrawList();
-
 	for (auto &zone: zones) {
-		if (!mathUtils::pointInTriangle(mousePos, zone.a, zone.b, zone.c))
+		if (!(mathUtils::pointInTriangle(ctx.mouse, zone.a, zone.b, zone.c)))
 			continue ;
 		draw->AddTriangleFilled(zone.a, zone.b, zone.c, IM_COL32(255, 0, 0, 50));
-		if (ImGui::IsMouseReleased(ImGuiMouseButton_Left)) {
-			if (auto payload = ImGui::GetDragDropPayload()) {
-				IPanel* panel = *static_cast<IPanel**>(payload->Data);
-				split(zone.dir, panel);
-			}
-		}
+		if (ctx.released)	{ split(zone.dir, panel); }
+		return (true);
 	}
-	bottomLeft = topLeft;
-	bottomRight = topRight;
-	topLeft = origin;
-	topRight = {origin.x + avail.x, origin.y};
-	if (mathUtils::pointInTriangle(mousePos, topLeft, bottomRight, bottomLeft) ||
-		mathUtils::pointInTriangle(mousePos, topLeft, topRight, bottomRight)) {
-		draw->AddRectFilled(topLeft, bottomRight, IM_COL32(255, 0, 0, 50));
-		if (ImGui::IsMouseReleased(ImGuiMouseButton_Left)) {
-			if (auto payload = ImGui::GetDragDropPayload()) {
-				IPanel* panel = *static_cast<IPanel**>(payload->Data);
-				panel->changeOwner(this);
-			}
-		}
+	return (false);
+}
+
+void	Dock::renderTabBarZone(const RenderDragDropContext &ctx,
+							ImDrawList *draw, IPanel *panel) {
+	auto	&rectMin = ctx.origin;
+	auto	&rectMax = ctx.topRight;
+	if (ctx.mouse.x >= rectMin.x && ctx.mouse.x <= rectMax.x &&
+		ctx.mouse.y >= rectMin.y && ctx.mouse.y <= rectMax.y) {
+		draw->AddRectFilled(rectMin, rectMax, IM_COL32(255, 0, 0, 50));
+		if (ctx.released)	{ panel->changeOwner(this); }
 	}
+}
+
+void	Dock::renderDragDrop(void) {
+	auto	*payload = ImGui::GetDragDropPayload();
+	if (!payload || !payload->IsDataType("TAB_MOVE"))
+		return ;
+
+	RenderDragDropContext	ctx;
+	IPanel	*panel = *static_cast<IPanel**>(payload->Data);
+	auto	draw = ImGui::GetForegroundDrawList();
+
+	if (renderTriangleZones(ctx, draw, panel))
+		return ;
+	renderTabBarZone(ctx, draw, panel);
 }
 
 void	Dock::renderPanels(Window *window) {
--- src/platform/ui/Dock.hpp
+++ src/platform/ui/Dock.hpp
@@ -5,7 +5,7 @@
 /*  Project: Hel Engine                                                       */
 /*  Created: 2026/03/16 10:30:58 by hle-hena                                  */
 /*                                                                            */
-/*  Last Modified: 2026/03/17 21:06:41                                        */
+/*  Last Modified: 2026/03/17 23:25:26                                        */
 /*             By: hle-hena                                                   */
 /*                                                                            */
 /*    -----                                                                   */
@@ -38,10 +38,25 @@ class	Dock {
 		void	split(Splitter::Dir dir, IPanel *splitPanel);
 		void	merge(void);
 
-		void	renderPanels(Window *window);
+		struct	RenderDragDropContext {
+			ImVec2	topLeft, topRight, bottomLeft, bottomRight, center;
+			ImVec2	origin;
+			ImVec2	mouse;
+			float	tabBarH;
+			bool	released;
+
+			RenderDragDropContext(void);
+		};
+
+		bool	renderTriangleZones(const RenderDragDropContext &ctx,
+								ImDrawList *draw, IPanel *panel);
+		void	renderTabBarZone(const RenderDragDropContext &ctx,
+								ImDrawList *draw, IPanel *panel);
 		void	renderDragDrop(void);
+		void	renderPanels(Window *window);
 		std::vector<IPanel *>	_panels{};
 
+		void	renderSplits(Window *window, ImVec2 size);
 		std::unique_ptr<Dock>	_childOne{nullptr};
 		std::unique_ptr<Dock>	_childTwo{nullptr};
 		float					_splitRatio{0.5f};
